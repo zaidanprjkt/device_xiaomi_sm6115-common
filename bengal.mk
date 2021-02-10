4c89ee96f85e3bca467de1e5d7bb043002cbfc6a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:06:22 +0100

    sm6115-common: Build QTI Framework detect libs
    Change-Id: Ic4db96f8bd5a19f4805bd9c40544ed51d0352c61

--- bengal.mk
+++ bengal.mk
@@ -184,3 +184,11 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
+
+# QMI
+PRODUCT_PACKAGES += \
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
