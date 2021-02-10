c8a1ce9e1d87504c6bfddd675e36c0e967d8008e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 21:58:55 +0100

    sm6115-common: Build bluetooth libs
    Change-Id: I999905b2b7a0f7fe8f808e88c7ab428f1d50b263

--- bengal.mk
+++ bengal.mk
@@ -56,6 +56,15 @@ PRODUCT_PACKAGES += \
     libvisualizer \
     libvolumelistener
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
