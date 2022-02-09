02409637dedae452586e21983931df0025cefec2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 21:24:08 +0100

    sm6115-common: Build keymaster libs
    Change-Id: Ibff346e7ebce6bf3941dc1dcfc383c6e193aa51b

--- bengal.mk
+++ bengal.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.bengal
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1.vendor
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
