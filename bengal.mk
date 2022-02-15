46520275000f95dcd35d8a0344c7e58b857d1c96
Robert Shih <robertshih@google.com>
2022-02-15 17:07:30 +0000

    sm6115-common: enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I431554dcbef014f8235f048ee062a218a2131f9c

--- bengal.mk
+++ bengal.mk
@@ -402,7 +402,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 PRODUCT_VENDOR_PROPERTIES += \
