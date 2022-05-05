bb9e274e858721b75716321b8130495f4e5feb27
Alexander Koskovich <akoskovich@pm.me>
2022-05-04 19:20:48 -0700

    sm6115-common: Remove ro.com.android.dataroaming override
     * build/make/target/product/full_base_telephony.mk
       24:    ro.com.android.dataroaming?=true
    
     * system_ext/etc/build.prop:ro.com.android.dataroaming=false
       vendor/build.prop:ro.com.android.dataroaming=true
    
       In QSSI builds the partition priority is: odm, vendor,
       product, system_ext, system.
    
    Change-Id: Ia90d73ce62a156e4b88731cffd4d5f7a6dd795e6
    Signed-off-by: hiirakuun <rktdnt@proton.me>

--- bengal.mk
+++ bengal.mk
@@ -786,8 +786,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.radio.mt_sms_ack=30 \
     persist.vendor.radio.procedure_bytes=SKIP \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1 \
-    ro.com.android.dataroaming=true
+    persist.vendor.radio.sib16_support=1
 
 # Sensors
 PRODUCT_PACKAGES += \
