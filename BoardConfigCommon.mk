188701eadbd544b1d0b5c9d54d104ac5f80a122c
HeroBuxx <herobuxx@gmail.com>
2022-05-31 23:16:18 +0700

    sm6115-common: sepolicy: Initial common sepolicies
    - Pull from Xiaomi SM8250
    
    Co-authored-by: Pig <pig.priv@gmail.com>
    Co-authored-by: Zinadin Zidan <zidan.roking@gmail.com>
    Co-authored-by: Utsav Balar <utsavbalar1231@gmail.com>
    Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
    Co-authored-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Co-authored-by: boedhack99 <boedhack@gmail.com>
    Co-authored-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Change-Id: Ibcc99f18bd514d105207ac0fd23355b7d0ea942f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,6 +135,8 @@ USE_SENSOR_MULTI_HAL := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
