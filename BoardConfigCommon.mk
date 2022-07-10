9a2cb84155ec19f35c9138cbb672bd59061aada5
xSylla <syllamodder@gmail.com>
2022-07-11 01:15:37 +0200

    sm6115-common: Enforce fstab suffix to qcom
    Change-Id: I5b7246196ec57020cdf980a4867b6b9b08ab3087

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,7 +78,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=default
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_KERNEL_BASE := 0x00000000
