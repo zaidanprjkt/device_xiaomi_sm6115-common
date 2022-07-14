3f9d308f242f6269ecf872f94ecb524560fb347d
Alexandru Scurtu <sasha.scurtu@gmail.com>
2022-07-14 15:58:36 +0700

    sm6115-common: Build sensorservice 1.0 HAL system variant
    * E init    : Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 516 (/system/bin/hwservicemanager)
    
    Change-Id: If696de35b0d70e1f67d72be456347c6c7fd691f7

--- bengal.mk
+++ bengal.mk
@@ -254,6 +254,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
