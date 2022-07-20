dcccc12d122703f8fc150301321eb1ca548df0ff
Alexander Koskovich <akoskovich@pm.me>
2022-07-20 14:41:25 -0700

    sm6115-common: Move Bluetooth power overlays to sysprops
     * These were moved to sysprops in Android 13 [1] and while
       frameworks reads the values directly from power_profile.xml
       Bluetooth app reads from the properties.
    
     [1] https://android.googlesource.com/platform/system/libsysprop/+/976d33062b51e77edde44faad1a6e801c5dc0188%5E%21/#F0
    
    Change-Id: I5bff67a15c537e8a47a19f30776fca6bf2c9fbe5

--- bengal.mk
+++ bengal.mk
@@ -234,6 +234,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     bluetooth.device.class_of_device=90,2,12 \
+    bluetooth.hardware.power.operating_voltage_mv=3300 \
     bluetooth.profile.a2dp.source.enabled?=true \
     bluetooth.profile.asha.central.enabled?=true \
     bluetooth.profile.avrcp.target.enabled?=true \
