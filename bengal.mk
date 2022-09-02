d4bbe8ba5d33f4ce3fbee0729aa8b5e2899d2b5e
LuK1337 <priv.luk@gmail.com>
2022-09-02 14:14:28 +0200

    sm6115-common: Pull appropriate USB props from vendor_product.mk
    Change-Id: I93b25c5d0989c83d1c8d7b7946a083771febdddd

--- bengal.mk
+++ bengal.mk
@@ -787,7 +787,13 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.usb.diag.func.name=diag \
-    vendor.usb.use_ffs_mtp=0
+    vendor.usb.dpl.inst.name=dpl \
+    vendor.usb.qdss.inst.name=qdss \
+    vendor.usb.rmnet.func.name=gsi \
+    vendor.usb.rmnet.inst.name=rmnet \
+    vendor.usb.rndis.func.name=gsi \
+    vendor.usb.use_ffs_mtp=0 \
+    vendor.usb.use_gadget_hal=0
 
 PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.oem.otg_support=true
