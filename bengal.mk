50c960c8e549a3c28aff0dc001818d93eeb42b3d
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-01-15 21:40:43 +0700

    sm6115-common: Import rootdir files from V13.0.2.0.SJQMIXM
    Change-Id: Id7dfa304eb90ab6ff6f13acfbcaeea494fbfada9

--- bengal.mk
+++ bengal.mk
@@ -254,6 +254,21 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    init.qti.dcvs.sh
+
+PRODUCT_PACKAGES += \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.target.rc \
+    fstab.qcom \
+    ueventd.qcom.rc
+
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
