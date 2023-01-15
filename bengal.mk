35825a0b4bc543fd0cad3d3d5d926cce35311ae8
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-01-15 21:45:26 +0700

    sm6115-common: Import public libraries list from V13.0.2.0.SJQMIXM
    Change-Id: I9762625394ac724b24060250e5bf4f59d9d341c1

--- bengal.mk
+++ bengal.mk
@@ -246,6 +246,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
