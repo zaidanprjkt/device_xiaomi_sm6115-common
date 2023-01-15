suckitdjiwa/android_device_xiaomi_sm6115-common

sm6115-common: Copy fstab to ramdisk
Change-Id: I17033687ee0cc92a84f5d045e85d7df80184233d

bengal.mk
@@ -282,6 +282,9 @@ PRODUCT_PACKAGES += \
     fstab.default \
     ueventd.qcom.rc
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+
 # Seccomp
 PRODUCT_COPY_FILES := \
    $(LOCAL_PATH)/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/configstore \

zaidanprjkt/device_xiaomi_sm6115-common

bengal.mk
@@ -269,6 +269,9 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     ueventd.qcom.rc
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \