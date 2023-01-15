963a31db4486d11981eff5eaaa8466b99533294d
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-01-15 19:11:17 +0700

    sm6115-common: Initial tree
    Change-Id: I832b660c20a1fa46836577ae7ce85b25d2c718e7

---
+++ Android.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter citrus lime,$(TARGET_DEVICE)),)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+endif
