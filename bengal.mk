296c75c0c0a7494f17eb0e2d31918e7cd9abd858
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-01-16 18:18:07 +0700

    sm6115-common: Import USB configs from V13.0.2.0.SJQMIXM
    Change-Id: If43b437b330e024e8c408113bec2c975b87015ac

--- bengal.mk
+++ bengal.mk
@@ -742,6 +742,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.xiaomi_bengal \
     android.hardware.usb.gadget@1.1-service.xiaomi_bengal
 
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.usb.diag.func.name=diag \
     vendor.usb.use_ffs_mtp=0
