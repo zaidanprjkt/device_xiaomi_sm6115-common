529644d3551e23ef5d42931f0bcdf367653465ba
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-01-16 21:00:56 +0700

    sm6115-common: Drop SELinux permissive flags
    Change-Id: Ie5b0a169671e41816186438aab57f07fc6d56d67

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,7 +82,6 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=default
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_OFFSET := 0x00008000
