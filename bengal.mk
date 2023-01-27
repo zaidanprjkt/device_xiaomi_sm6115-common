d214347cf9c6a5b003779d20bee6c39e19471883
hiirakuun <rktdnt@proton.me>
2023-01-27 15:36:59 +0700

    sm6115-common: Drop USB Gadget Implementation
    * Causing MTP broken
    
    Signed-off-by: hiirakuun <rktdnt@proton.me>

--- bengal.mk
+++ bengal.mk
@@ -783,8 +783,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.xiaomi_bengal \
-    android.hardware.usb.gadget@1.1-service.xiaomi_bengal
+    android.hardware.usb@1.3-service.xiaomi_bengal
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
