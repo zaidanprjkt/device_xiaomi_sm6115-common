8c8c81ade8bb1d46841df4c75bcdf1bfcd629469
hiirakuun <rktdnt@proton.me>
2023-01-29 15:30:10 +0700

    Revert "sm6115-common: Use libhidlbase from vndk v32 for vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so"
    This reverts commit 95e1170414850e6240d653f678362387174af97c.
    
    Signed-off-by: hiirakuun <rktdnt@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -60,13 +60,11 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
-            "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
-            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+        vendor/lib/libsample5.so)
+            "${PATCHELF}" --replace-needed "libsample6.so" "libsample7.so" "${2}"
             ;;
-        vendor/lib64/com.fingerprints.extension@1.0.so)
-            "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
-            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+        vendor/lib/libsample7.so)
+            "${PATCHELF}" --set-soname "libsample7.so" "${2}"
             ;;
     esac
 }
