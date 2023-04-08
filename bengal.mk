e454f9a65eb9596fa7729f16703caa39e3bfe942
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-04-08 20:44:16 +0700

    sm6115-common: Drop cdfinger keylayout
     * Not used.
    
    Change-Id: I4ae92a2e039f5c7cd5b6f4d4945f9b5fa66fd02a
    Signed-off-by: twenfourl <rktdnt@proton.me>

--- bengal.mk
+++ bengal.mk
@@ -562,7 +562,6 @@ PRODUCT_PACKAGES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/uinput-cdfinger.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-cdfinger.kl \
     $(LOCAL_PATH)/keylayout/uinput-focal.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-focal.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
