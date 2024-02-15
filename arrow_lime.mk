1d891cb52496bc80d5fe8b97edd7cdc36b3d5c67
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-02-15 08:43:45 +0700

    ArrowOS Bringup

--- arrow_lime.mk
+++ arrow_lime.mk
@@ -8,20 +8,12 @@
 $(call inherit-product, device/xiaomi/chime/device-lime.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_lime
+PRODUCT_NAME := arrow_lime
 PRODUCT_DEVICE := lime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 9T
 PRODUCT_MANUFACTURER := Xiaomi
 
-# RisingOS flags.
-RISING_MAINTAINER := scaledzdn.
-RISING_OFFICIAL := true
-RISING_CHIPSET := SD662
-TARGET_ENABLE_BLUR := false
-TARGET_HAS_UDFPS := false
-TARGET_USE_PIXEL_FINGERPRINT := false
-TARGET_OPTOUT_GOOGLE_TELEPHONY := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 
