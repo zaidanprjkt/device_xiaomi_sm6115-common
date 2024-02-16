a4bad1c766fd571fe5386676280afc8fde196aef
zaidanmovic <zaidanmovic0512@gmail.com>
2024-02-16 00:35:16 +0000

    chime: Speed profile services and wifi-service to reduce RAM and storage Co-Authored-By: Mathieu Chartier <mathieuc@google.com>
    Signed-off-by: zaidanmovic <zaidanmovic0512@gmail.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/xiaomi/chime/custom.mk)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
