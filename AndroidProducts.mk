528c207447b916bdca5d155cdcb7eb8cb0b033d8
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-02-19 19:46:32 +0700

    chime: Change to rising on AndroidProduct

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,12 +6,12 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_citrus.mk \
-    $(LOCAL_DIR)/lineage_lime.mk
+    $(LOCAL_DIR)/rising_lime.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_citrus-user \
     lineage_citrus-userdebug \
     lineage_citrus-eng \
-    lineage_lime-user \
-    lineage_lime-userdebug \
-    lineage_lime-eng
+    rising_lime-user \
+    rising_lime-userdebug \
+    rising_lime-eng
